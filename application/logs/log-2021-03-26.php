35858af78afe85ae9cb1d63593ca101c4a66ea53
SHANJOY SHAJAN <shanjoys@gmail.com>
2021-03-27 03:20:19 +0530

    Initial commit

---
+++ application/logs/log-2021-03-26.php
@@ -0,0 +1,53 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-26 11:13:24 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\ecommercefinal\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-03-26 11:13:24 --> Unable to connect to the database
+ERROR - 2021-03-26 11:13:33 --> Query error: Table 'shop.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock' LIMIT 1
+ERROR - 2021-03-26 11:30:59 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:04 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:18 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:32 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:34 --> Severity: Notice --> Trying to access array offset on value of type int C:\xampp\htdocs\ecommercefinal\application\views\templates\redlabel\checkout.php 6
+ERROR - 2021-03-26 11:32:34 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:38 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:39 --> Severity: Notice --> Trying to access array offset on value of type int C:\xampp\htdocs\ecommercefinal\application\views\templates\redlabel\shopping_cart.php 8
+ERROR - 2021-03-26 11:32:40 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:41 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:32:48 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:35:34 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:35:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:35:47 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:36:36 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2021-03-26 11:36:45 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:36:53 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:37:50 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:37:53 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:37:54 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:53:31 --> Severity: Notice --> Trying to access array offset on value of type int C:\xampp\htdocs\ecommercefinal\application\views\templates\greenlabel\_parts\header.php 218
+ERROR - 2021-03-26 11:53:31 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:53:31 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:53:32 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:53:54 --> Severity: Notice --> Trying to access array offset on value of type int C:\xampp\htdocs\ecommercefinal\application\views\templates\clothesshop\_parts\header.php 94
+ERROR - 2021-03-26 11:53:54 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:54:12 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:54:18 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:54:18 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 11:54:18 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:02:19 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:37:37 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 12:38:14 --> 404 Page Not Found: /index
+ERROR - 2021-03-26 22:46:15 --> 404 Page Not Found: /index
---
+++ application/modules/admin/views/home/login.php
@@ -0,0 +1,41 @@
+<style>
+    body {
+        background-image:url('/assets/imgs/login-bg.png');
+        background-position: bottom  right;
+        background-repeat: no-repeat;
+        background-color:#548fd0;
+    }
+    .avatar {background-image:url('/assets/imgs/login-cover.png')}
+</style>
+ <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Askmee - Admin</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="assets/css/app.min.css">
+  <link rel="stylesheet" href="assets/bundles/bootstrap-social/bootstrap-social.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+<div class="container">
+    <div class="login-container">
+        <div id="output">       
+            <?php
+            if ($this->session->flashdata('err_login')) {
+                ?>
+                <div class="alert alert-danger"><?= $this->session->flashdata('err_login') ?></div>
+                <?php
+            }
+            ?></div>
+        <div class="avatar"></div>
+        <div class="form-box">
+            <form action="" method="POST">
+                <input type="text" name="username" placeholder="username">
+                <input type="password" name="password" placeholder="password">
+                <button class="btn btn-info btn-block login" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
